1caaaf1b5841df2254910c0870ff35044c2e2fdd
Fi <158217226+lightfi7@users.noreply.github.com>
2024-08-03 13:43:32 -0700

    Update EmailIsVerified.php

--- app/Http/Middleware/EmailIsVerified.php
+++ app/Http/Middleware/EmailIsVerified.php
@@ -22,19 +22,19 @@ class EmailIsVerified
     public function handle(Request $request, Closure $next, $redirectToRoute = null)
     {
         $user = $request->user();
-        if(!$user->email_verified_at) {
-            $otp = Otp::setValidity( 30 )  // otp validity time in mins
-                ->setLength( 8 )  // Lenght of the generated otp
-                ->setMaximumOtpsAllowed( 10 ) // Number of times allowed to regenerate otps
-                ->setOnlyDigits( true )  // generated otp contains mixed characters ex:ad2312
-                ->setUseSameToken( false ) // if you re-generate OTP, you will get same token
-                ->generate( $user->email );
-            // send mail
-            Mail::to( $user->email )->send( new SendCodeMail( $otp->token ) );
-            return $request->expectsJson()
-                    ? abort(403, 'Your email address is not verified.')
-                    : Redirect::guest('/email/verify');
-        }
+        // if(!$user->email_verified_at) {
+        //     $otp = Otp::setValidity( 30 )  // otp validity time in mins
+        //         ->setLength( 8 )  // Lenght of the generated otp
+        //         ->setMaximumOtpsAllowed( 10 ) // Number of times allowed to regenerate otps
+        //         ->setOnlyDigits( true )  // generated otp contains mixed characters ex:ad2312
+        //         ->setUseSameToken( false ) // if you re-generate OTP, you will get same token
+        //         ->generate( $user->email );
+        //     // send mail
+        //     Mail::to( $user->email )->send( new SendCodeMail( $otp->token ) );
+        //     return $request->expectsJson()
+        //             ? abort(403, 'Your email address is not verified.')
+        //             : Redirect::guest('/email/verify');
+        // }
         return $next($request);
     }
 }
